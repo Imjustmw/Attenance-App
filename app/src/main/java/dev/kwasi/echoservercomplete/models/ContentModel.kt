19816103b0112d8891f6b12175eab51925ad7cec
Jonathan <jonathan.joseph2@my.uwi.edu>
2024-10-17 08:08:35 -0400

    First Commit to Student App

--- app/src/main/java/dev/kwasi/echoservercomplete/models/ContentModel.kt
+++ app/src/main/java/dev/kwasi/echoservercomplete/models/ContentModel.kt
@@ -1,5 +1,12 @@
 package dev.kwasi.echoservercomplete.models
 
+import java.sql.Timestamp
+
 /// The [ContentModel] class represents data that is transferred between devices when multiple
 /// devices communicate with each other.
-data class ContentModel(val message:String, var senderIp:String, var studentId: String)
+data class ContentModel(
+    val message:String,
+    var senderIp:String,
+    var studentId: String,
+    var timestamp: Long = System.currentTimeMillis()
+)
